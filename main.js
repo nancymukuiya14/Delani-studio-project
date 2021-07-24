96a95f30646efa7adeec73d6897fa73acf08cf29
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-07-24 18:01:35 +0300

    update my main.js file

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+        $("#design").click(function () {
+            $(".designimage").toggle();
+            $(".defination").toggle();
+        });
+        $("#development").click(function () {
+            $(".dvtimage").toggle();
+            $(".defination2").toggle();
+        });
+        $("#product").click(function () {
+            $(".productimage").toggle();
+            $(".defination3").toggle();
+        });
+    });
+
