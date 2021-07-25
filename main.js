4d483625456d0f3a9e6247c0ce1c7ae4570c395e
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-07-25 14:25:06 +0300

    indent my css file

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
             $(".defination3").toggle();
         });
 
+
         // contact form validation//
    //adding event listener//
 var btn= document.getElementById("send-message");
